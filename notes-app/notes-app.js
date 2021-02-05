3ceff05a52283baa66b1b8ff150c7ec49de42664
Jack East <jackeast23@gmail.com>
2021-02-04 22:36:17 -0500

    added sorting function

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,17 +1,22 @@
 let notes = getSavedNotes()
 
 const filters = {
-    searchText: ''
+    searchText: '',
+    sortBy: 'byEdited'
 }
 
 renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click', function (e) {
     const id = uuidv4()
+    const timestamp = moment().valueOf()
+
     notes.push({
         id: id,
         title: '',
-        body: ''
+        body: '',
+        createdAt: timestamp,
+        updatedAt: timestamp
     })
     saveNotes(notes)
     location.assign(`/edit.html#${id}`)
@@ -23,7 +28,8 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
 })
 
 document.querySelector('#filter-by').addEventListener('change', function (e) {
-    console.log(e.target.value)
+    filters.sortBy = e.target.value
+    renderNotes(notes, filters)
 })
 
 window.addEventListener('storage', function (e) {
@@ -32,30 +38,3 @@ window.addEventListener('storage', function (e) {
         renderNotes(notes, filters)
     }
 })
-
-// Unix Epoch - January 1st 1970 00:00:00
-
-const now = new Date()
-const timestamp = now.getTime()
-
-const myDate = new Date(timestamp)
-console.log(myDate.getFullYear())
-
-// console.log(`Year: ${now.getFullYear()}`)
-// console.log(`Month: ${now.getMonth()}`)
-// console.log(`Day: ${now.getDate()}`)
-// console.log(`Hour: ${now.getHours()}`)
-// console.log(`Minutes: ${now.getMinutes()}`)
-// console.log(`Seconds: ${now.getSeconds()}`)
-
-const dateOne = new Date('June 15 1993 8:45:00')
-const dateTwo = new Date('May 31 1995 11:59:00')
-
-const timestampOne = dateOne.getTime()
-const timestampTwo = dateTwo.getTime()
-
-if (timestampOne > timestampTwo) {
-    console.log(dateTwo.toString())
-} else if (timestampOne < timestampTwo) {
-    console.log(dateOne.toString())
-}
--- notes-app/notes-functions.js
+++ notes-app/notes-functions.js
@@ -45,6 +45,7 @@ const generateNoteDOM = function (note) {
         textEl.textContent = note.title
     } else {
         textEl.textContent = 'Unamed note'
+        note.title = 'Unnamed note' // added to account for edge case that caused unamed note to appear first when sorting alphabetically
     }
     textEl.setAttribute('href', `/edit.html#${note.id}`)
     noteEl.appendChild(textEl)
@@ -52,8 +53,46 @@ const generateNoteDOM = function (note) {
     return noteEl
 }
 
+// Sort your notes by one of three ways
+const sortNotes = function (notes, sortBy) {
+    if (sortBy === 'byEdited') {
+        return notes.sort(function (a, b) {
+            if (a.updatedAt > b.updatedAt) {
+                return -1
+            } else if (a.updatedAt < b.updatedAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'byCreated') {
+        return notes.sort(function (a, b) {
+            if (a.createdAt > b.createdAt) {
+                return -1
+            } else if (a.createdAt < b.createdAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'alphabetical') {
+        return notes.sort(function (a, b) {
+            if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                return -1
+            } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else {
+        return notes
+    }
+}
+
 // Render application notes
 const renderNotes = function (notes, filters) {
+    notes = sortNotes(notes, filters.sortBy)
     const filteredNotes = notes.filter(function (note) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
     })
@@ -64,4 +103,9 @@ const renderNotes = function (notes, filters) {
        const noteEl = generateNoteDOM(note)
         document.querySelector('#notes').appendChild(noteEl)
     })
+}
+
+// Generate the last edited message
+const generateLastEdited = function (timestamp) {
+    return `Last edited ${moment(timestamp).fromNow()}`
 }
